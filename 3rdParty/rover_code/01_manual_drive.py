c020c973787af292568bcac09d6c6036a6f888c6
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-03-23 11:09:42 +0100

    fix: big refactoring for my little robot

--- 3rdParty/rover_code/01_manual_drive.py
+++ 3rdParty/rover_code/01_manual_drive.py
@@ -13,16 +13,22 @@ FIELD_WIDTH = 2000
 FIELD_HEIGHT = 1000
 FIELD_BOUNDARY = 100
 DEFAULT_THRESHOLD = 50
+FINISH_VALUE = 235
+
+
+highest_point = [-1, -1, MEASUREMENT_DEFAULT_VALUE]
+
+######################################
+# REMOTE_CONTROL
+######################################
 
 
 def turn_right(bot: Rover):
     bot.set_motor_speed(TURNING_SPEED, -TURNING_SPEED)
-    # time.sleep(TURNING_DURATION)
 
 
 def turn_left(bot: Rover):
     bot.set_motor_speed(-TURNING_SPEED, TURNING_SPEED)
-    # time.sleep(TURNING_DURATION)
 
 
 def remote_control(bot: Rover):
@@ -45,6 +51,10 @@ def remote_control(bot: Rover):
                 measure(bot)
                 print(bot.measurement)
 
+######################################
+# HELPER_FUNCTIONS
+######################################
+
 
 def measure(bot):
     bot.measurement = MEASUREMENT_DEFAULT_VALUE
@@ -54,29 +64,6 @@ def measure(bot):
         time.sleep(1)
 
     return bot.measurement
-
-
-def generate_terrain(seed: int):
-    shape = (200, 200)
-    noise = PerlinNoise(octaves=2, seed=seed)
-
-    world = [[noise([i/shape[0], j/shape[1]])
-              for j in range(shape[0])] for i in range(shape[1])]
-
-    lin_x = np.linspace(0, 1, shape[0], endpoint=False)
-    lin_y = np.linspace(0, 1, shape[1], endpoint=False)
-    x, y = np.meshgrid(lin_x, lin_y)
-
-    return (x, y, world)
-
-
-def drawColorPlot(x, y, z):
-    color_count = 70
-
-    fig = plt.Figure()
-    plt.contourf(x, y, z, color_count, cmap="hot")
-    plt.colorbar()
-    plt.show()
 
 
 def get_random_points_in_field(point_count):
@@ -99,36 +86,13 @@ def explore_points_and_get_list_with_concentration(bot: Rover, point_list):
     return result_list
 
 
-def measure_and_update_values(bot: Rover):
-    time.sleep(1)
-    measure(bot)
-
-    result = [bot.position["x"], bot.position["y"], bot.measurement]
-    return result
-
-
-highest_point = [-1, -1, -1]
-
-
-def update_highest_point(new_point):
+def isNewHighestPoint(point):
     global highest_point
-    if new_point[2] > highest_point[2]:
-        highest_point = new_point
+    return has_higher_concentration(point, highest_point)
 
-
-def simplex(bot: Rover, p1):
-    global highest_point
-
-    p2 = [p1[0] + 300, p1[1], -1]
-    p3 = [p1[0] + 150, p1[1] + 150, -1]
-
-    bot.move_to(p2, threshold=DEFAULT_THRESHOLD)
-    p2 = measure_and_update_values(bot)
-    bot.move_to(p3, threshold=DEFAULT_THRESHOLD)
-    p3 = measure_and_update_values(bot)
 
 def get_point_with_highest_concentration(points_list):
-    result = [-1, -1, -1]
+    result = [-1, -1, MEASUREMENT_DEFAULT_VALUE]
     for point in points_list:
         concentration = point[2]
         if (concentration <= result[2]):
@@ -139,105 +103,152 @@ def get_point_with_highest_concentration(points_list):
     return result
 
 
-def simplex(p1, p2, p3):
+def stop(bot: Rover):
+    bot.set_motor_speed(0, 0)
+    time.sleep(1)
+    bot.set_leds(0, 0, 0)
+
+    print(bot.position)
+    print(bot.acceleration)
+    print(bot.status)
+
+    bot.beep()
+
+    bot.disconnect()
+
+
+def get_real_values(bot: Rover, point):
+    bot.move_to(point, threshold=DEFAULT_THRESHOLD)
+    time.sleep(1)
+    measure(bot)
+
+    result = [bot.position["x"], bot.position["y"], bot.measurement]
+    if (not isNewHighestPoint(result)):
+        return result
+
+    highest_point = result
+    if has_higher_concentration(highest_point, FINISH_VALUE):
+        stop(bot)
+        exit()
+
+
+def get_ascended_sorted_list(points_list):
+    return sorted(points_list, key=lambda tupel: tupel[2], reverse=True)
+
+
+def get_middle_point(p1, p2):
+    result = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) /
+              2, MEASUREMENT_DEFAULT_VALUE]
+    return result
+
+
+def calculate_special_point(p1, p2, constant):
+    result = [p1[0] + constant * (p1[0] - p2[0]),
+              p1[1] + constant * (p1[1] - p2[1]), MEASUREMENT_DEFAULT_VALUE]
+    return result
+
+
+def has_higher_concentration(p1, p2):
+    result = p1[2] > p2[2]
+    return result
+
+
+def calculate_contraction_point(p1, p2, constant):
+    result = [p1[0] + constant * (p2[0] - p1[0]),
+              p1[1] + constant * (p2[1] - p1[1]), MEASUREMENT_DEFAULT_VALUE]
+    return result
+
+
+def simplex(bot: Rover, p1):
+    global highest_point
+
+    ALPHA = 1.3
+    BETA = 0.75
+    GAMMA = 1.25
+    DELTA = 0.5
+
+    exact_p2 = [p1[0] + 300, p1[1], MEASUREMENT_DEFAULT_VALUE]
+    exact_p3 = [p1[0] + 150, p1[1] + 150, MEASUREMENT_DEFAULT_VALUE]
+
+    p2 = get_real_values(bot, exact_p2)
+    p3 = get_real_values(bot, exact_p3)
+
     point_list = [p1, p2, p3]
     print("point_list", point_list)
 
-    while highest_point[2] < 250:
+    while True:
         print("Highest Point", str(highest_point))
-        # sortieren der Punkte
-        sorted_list = sorted(
-            point_list, key=lambda tupel: tupel[2], reverse=True)
+
+        sorted_list = get_ascended_sorted_list(point_list)
         print("sorted_list ", sorted_list)
 
-        update_highest_point(sorted_list[0])
-
-        # Mittelpunkt der zwei besten Werte
         best_point = sorted_list[0]
         second_best = sorted_list[1]
         worst_point = sorted_list[2]
-        middle_point_of_2_best_points = [(best_point[0] + second_best[0]) / 2,
-                                         (best_point[1] + second_best[1]) / 2,]
+
+        middle_point_of_2_best_points = get_middle_point(
+            best_point, second_best)
         print("middle_point_of_2_best_points ", middle_point_of_2_best_points)
 
-        # reflexion
-        alpha = 1.25
-        reflection_point = [middle_point_of_2_best_points[0] + alpha *
-                            (middle_point_of_2_best_points[0] -
-                             worst_point[0]),
-                            middle_point_of_2_best_points[1] + alpha *
-                            (middle_point_of_2_best_points[1] -
-                             worst_point[1]), -1
-                            ]
-        bot.move_to(reflection_point, threshold=DEFAULT_THRESHOLD)
-        reflection_point = measure_and_update_values(bot)
+        exact_reflection_point = calculate_special_point(
+            middle_point_of_2_best_points, worst_point, ALPHA)
+        reflection_point = get_real_values(bot, exact_reflection_point)
         print("reflection_point", reflection_point)
-        update_highest_point(reflection_point)
 
-        if reflection_point[2] > best_point[2]:
-            gamma = 1.25
-            expand_point = [reflection_point[0] + gamma * (reflection_point[0] - middle_point_of_2_best_points[0]),
-                            reflection_point[1] + gamma *
-                            (reflection_point[1] -
-                             middle_point_of_2_best_points[1]),
-                            -1]
-            bot.move_to(expand_point, threshold=DEFAULT_THRESHOLD)
-            expand_point = measure_and_update_values(bot)
+        if has_higher_concentration(reflection_point, best_point):
+            exact_expand_point = calculate_special_point(
+                reflection_point, middle_point_of_2_best_points, GAMMA)
+            expand_point = get_real_values(bot, exact_expand_point)
             print("expand_point", expand_point)
-            update_highest_point(expand_point)
 
-            if expand_point[2] > reflection_point[2]:
+            if has_higher_concentration(expand_point, reflection_point):
                 worst_point = expand_point
-            else:
-                worst_point = reflection_point
+                continue
+
+            worst_point = reflection_point
             continue
-        elif reflection_point[2] > second_best[2]:
+        elif has_higher_concentration(reflection_point, second_best):
             worst_point = reflection_point
             continue
 
-        beta = 0.75
-        h = reflection_point if reflection_point[2] > worst_point[2] else worst_point
-        contracting_point = [h[0] + beta * (middle_point_of_2_best_points[0] - h[0]),
-                             h[1] + beta * (middle_point_of_2_best_points[1] - h[1]), -1]
-        bot.move_to(contracting_point, threshold=DEFAULT_THRESHOLD)
-        contracting_point = measure_and_update_values(bot)
+        h = reflection_point if has_higher_concentration(
+            reflection_point, worst_point) else worst_point
+        exact_contracting_point = calculate_contraction_point(
+            h, middle_point_of_2_best_points, BETA)
+        contracting_point = get_real_values(bot, exact_contracting_point)
         print("contracting_point", contracting_point)
-        update_highest_point(contracting_point)
 
-        if contracting_point[2] > worst_point[2]:
+        if has_higher_concentration(contracting_point, worst_point):
             worst_point = contracting_point
             continue
 
         # for item in sorted_list:
-        #     delta = 0.5
-        #     item += delta * ( best_point - item)
+        #     DELTA = 0.5
+        #     item += DELTA * ( best_point - item)
 
 
+######################################
+# MAIN_SCRIPT
+######################################
 bot = Rover()
 bot.beep()
 
+bot.set_leds(0, 0, 30)
+
 # remote_control(bot)
-point_list = get_random_points_in_field(2)
-print("Generated Points:" + str(point_list))
+# point_list = get_random_points_in_field(2)
+# print("Generated Points:" + str(point_list))
 
-explored_points_list = explore_points_and_get_list_with_concentration(
-    bot, point_list)
-print("Explored Points:" + str(explored_points_list))
+# explored_points_list = explore_points_and_get_list_with_concentration(
+#     bot, point_list)
+# print("Explored Points:" + str(explored_points_list))
 
-explored_point = (1105, 452, 101)
-max_point = get_point_with_highest_concentration(explored_points_list)
-print(max_point)
-# simplex(max_point)
-
-bot.move_to(explored_point)
+# explored_point = [1105, 452, 101]
+explored_point = [967, 493, 84]
+# max_point = get_point_with_highest_concentration(explored_points_list)
+# print(max_point)
 time.sleep(1)
 simplex(bot, explored_point)
 
-
-bot.set_motor_speed(0, 0)
-time.sleep(1)
 bot.set_leds(0, 0, 0)
-
-# print(bot.position)
-# print(bot.acceleration)
-# print(bot.status)
+stop(bot)
